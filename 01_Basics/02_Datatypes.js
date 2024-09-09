f6a296aa4fdd8f7d31f9e13c3318951b782a8b15
Deeptimayee ojha <ojhadeeptymayee@gmail.com>
2024-09-09 20:05:03 +0530

    Datatype

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -1,1 +1,9 @@
 "use strict"; // treat all js code as newer version
+
+//alert( 3 + 3 ) // we are using nodejs not browser
+console.log(3 + 3); //code readability
+
+//null => standalone value
+console.log(typeof "deepti");
+console.log(typeof null); //it is an object
+console.log(typeof undefined); // it is undefined
