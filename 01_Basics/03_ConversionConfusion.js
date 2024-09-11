f128c96e6caa1ccf8e76dcfd95a0b25cd631e302
Deeptimayee ojha <ojhadeeptymayee@gmail.com>
2024-09-11 20:03:37 +0530

    ConfusiningConparision

--- 01_Basics/03_ConversionConfusion.js
+++ 01_Basics/03_ConversionConfusion.js
@@ -1,29 +1,61 @@
-let score = "33";
+// let score = "33";
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //let score1 = "33abc";
 // let score1 = null;
-let score1 = true;
-console.log(typeof score1);
+// let score1 = true;
+// console.log(typeof score1);
 
-let valueInNumber1 = Number(score1);
-console.log(typeof valueInNumber1); //string---->number, null--> object, boolean--> 1(true)/0(false), number--> number
-console.log(valueInNumber1); //33abc ---> NaN ,null --->0 ,true --->1
+// let valueInNumber1 = Number(score1);
+// console.log(typeof valueInNumber1); //string---->number, null--> object, boolean--> 1(true)/0(false), number--> number
+// console.log(valueInNumber1); //33abc ---> NaN ,null --->0 ,true --->1
 /*
 string---->number, null--> object, boolean--> 1(true)/0(false), number--> number
 33abc ---> NaN ,null --->0 ,true --->1
  */
 
-let num = 1;
-let booleanIsLogged = Boolean(num);
-console.log(booleanIsLogged);
+//let num = 1;
+// let num = "";
+// let booleanIsLogged = Boolean(num);
+// console.log(booleanIsLogged);
 
 /*
 (i/p) 1 --> (0/p)true
+string, ""--> true, false
 */
+
+// let n = 5;
+// let str = String(n);
+// console.log(str);
+// console.log(typeof str);
+
+//*************************operations**************************/
+
+// let value = 5;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.table([2 + 2, 2 - 2, 2 * 2]);
+
+// let str1 = "deeptimayee";
+// let str2 = "ojha";
+// let res = str1 + " " + str2;
+// console.log(res);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true+); //error
+console.log(+true);
+console.log(+"");
+
+// google search => prefix and postfix js mdn
+// conversion => ecma script (39) abstract operations
